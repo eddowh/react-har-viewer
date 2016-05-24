863ffb74596bb940bb35a88da6345020dc514a32
Eddo Hintoso <eddo.hintoso@gmail.com>
2016-05-24 14:41:48 +0800

    Set up NPM and Webpack

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+/**
+ * Entry point
+ */
+
+
+// React
+import React from 'react';
+
+// Stylesheets
+require('./app.scss');
+
+
+React.render(
+  <h1>React Deep Dive</h1>,
+  document.body
+);
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+/**
+ * Not production!
+ */
+
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+
+  // input file
+  entry: {
+    app: ['./src/app.jsx']
+  },
+
+  output: {
+    path: './build',
+    filename: '[name].bundle.js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style!' + 'css?sourceMap'
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style!' + 'css?sourceMap' + '!sass?sourceMap'
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader?optional[]=runtime'
+      },
+      {
+        test: /\.(json)$/,
+        exclude: /node_modules/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.(svg|ttf|woff|woff2|eot)(\?v=\d\.\d+\.\d+)?$/,
+        loader: 'url-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'HAR Viewer (React Deep Dive)'
+    })
+  ],
+
+  devtool: 'source-map'
+
+}
